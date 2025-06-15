ae9cb8d5a1727d4f72e469565b50f1b5eae0d392
samuelshany <samuelhany345@gmail.com>
2025-06-15 20:52:56 +0300

    master auth routes

--- Modules/Auth/routes/api.php
+++ Modules/Auth/routes/api.php
@@ -18,3 +18,11 @@ Route::prefix('v1/auth')->group(function () {
     });
 });
 });
+Route::prefix('v1/auth/master')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('logout', [AuthController::class, 'logout']);
+    });
+});
