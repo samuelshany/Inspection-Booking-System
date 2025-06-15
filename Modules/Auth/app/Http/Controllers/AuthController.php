057c7133fdc5ff6baec1620bbb675d42f99eb4bc
samuelshany <samuelhany345@gmail.com>
2025-06-15 21:03:28 +0300

    add role column in users table

--- Modules/Auth/app/Http/Controllers/AuthController.php
+++ Modules/Auth/app/Http/Controllers/AuthController.php
@@ -22,6 +22,7 @@ class AuthController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8',
+            'role' => 'required|in:admin,user', // Default role for the first user
         ]);
 
         if ($validator->fails()) {
@@ -41,6 +42,7 @@ class AuthController extends Controller
                 'name' => $request->name,
                 'email' => $request->email,
                 'password' => Hash::make($request->password),
+                'role' => $request->role // Default role for the first user
             ]);
 
             $token = $user->createToken('auth_token')->plainTextToken;
